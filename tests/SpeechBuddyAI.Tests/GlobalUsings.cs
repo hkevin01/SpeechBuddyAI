b2d3c3d387fd9178040cc61edd8970aa19c65d9f
hkevin01 <kevin.hildebrand@gmail.com>
2026-07-01 06:32:38 -0400

    feat: Remove unused stub files from project references

---
+++ tests/SpeechBuddyAI.Tests/GlobalUsings.cs
@@ -0,0 +1,10 @@
+global using Xunit;
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using SpeechBuddyAI.Models;
+global using SpeechBuddyAI.Services;
+global using SpeechBuddyAI.Services.Confidence;
--- tests/SpeechBuddyAI.Tests/Stubs/MauiStubs.cs
+++ tests/SpeechBuddyAI.Tests/Stubs/MauiStubs.cs
@@ -1,42 +1,44 @@
-namespace Microsoft.Maui.Storage;
-
-public static class FileSystem
+namespace Microsoft.Maui.Storage
 {
-    public static string AppDataDirectory { get; set; } = Path.GetTempPath();
-}
-
-namespace Microsoft.Maui.ApplicationModel.DataTransfer;
-
-public interface IShare
-{
-    Task RequestAsync(ShareFileRequest request);
-}
-
-public sealed class ShareFile
-{
-    public ShareFile(string fullPath)
+    public static class FileSystem
     {
-        FullPath = fullPath;
+        public static string AppDataDirectory { get; set; } = Path.GetTempPath();
+    }
+}
+
+namespace Microsoft.Maui.ApplicationModel.DataTransfer
+{
+    public interface IShare
+    {
+        Task RequestAsync(ShareFileRequest request);
     }
 
-    public string FullPath { get; }
-}
-
-public sealed class ShareFileRequest
-{
-    public string? Title { get; set; }
-    public ShareFile? File { get; set; }
-}
-
-public static class Share
-{
-    public static IShare Default { get; set; } = new NoOpShare();
-
-    private sealed class NoOpShare : IShare
+    public sealed class ShareFile
     {
-        public Task RequestAsync(ShareFileRequest request)
+        public ShareFile(string fullPath)
         {
-            return Task.CompletedTask;
+            FullPath = fullPath;
+        }
+
+        public string FullPath { get; }
+    }
+
+    public sealed class ShareFileRequest
+    {
+        public string? Title { get; set; }
+        public ShareFile? File { get; set; }
+    }
+
+    public static class Share
+    {
+        public static IShare Default { get; set; } = new NoOpShare();
+
+        private sealed class NoOpShare : IShare
+        {
+            public Task RequestAsync(ShareFileRequest request)
+            {
+                return Task.CompletedTask;
+            }
         }
     }
 }
